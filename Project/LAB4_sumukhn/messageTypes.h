75dbe9e76bea6a86c5fe538ab26df5bd13bb6fa4
Sumukh Narendra <sumukhn@plus.seas.upenn.edu>
2013-11-26 13:06:15 -0500

    Creating a demo

---
+++ Project/LAB4_sumukhn/messageTypes.h
@@ -0,0 +1,12 @@
+#include <include.h>
+#include <ulib.h>
+#include <stdio.h>
+#include <string.h>
+
+enum Message{
+	SLAVE_DISCOVERY=1,
+	NEIGHBOR_MSG=2,
+	SENSOR_DATA = 3,
+	SENSOR_DATA_ACK = 4
+};
+
